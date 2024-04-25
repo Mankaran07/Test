180faa19e49cd836e9ea919528eb8fccbd6687f9
Josheen Kour <mankaransingh39@gmail.com>
2024-04-26 00:33:02 +0530

    For testing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. This website is of
-          Learning Saints..
+          Learning Saints.. This website is made for testing purposes.
         </p>
         <a
           className="App-link"
